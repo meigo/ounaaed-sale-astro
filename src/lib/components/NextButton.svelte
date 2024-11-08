edcd3a55f4e9c66ada7601d2acc9e0230cf30e55
Meigo Kukk <meigo@animal.ee>
2024-11-08 14:48:42 +0200

    u

---
+++ src/lib/components/NextButton.svelte
@@ -0,0 +1,11 @@
+<script>
+  let { url } = $props();
+  //  let open = $state(false);
+
+  function next() {
+    window.location.replace(url);
+    window.history.replaceState(nextState, nextTitle, nextURL);
+  }
+</script>
+
+<button type="button" onclick={next}> Next-- </button>
